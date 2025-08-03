4a1868ffd37f0f8eacc2da93498717765f2b4ae8
Dax Raad <d@ironbay.co>
2025-08-03 12:30:22 -0400

    Snapshot release v0.0.0-202508031629

--- script/publish.ts
+++ script/publish.ts
@@ -18,9 +18,7 @@ const pkgjsons = await Array.fromAsync(
 )
 
 const tree = await $`git add . && git write-tree`.text().then((x) => x.trim())
-for await (const file of new Bun.Glob("**/package.json").scan({
-  absolute: true,
-})) {
+for (const file of pkgjsons) {
   let pkg = await Bun.file(file).text()
   pkg = pkg.replaceAll(/"version": "[^"]+"/g, `"version": "${version}"`)
   await Bun.file(file).write(pkg)
@@ -43,9 +41,7 @@ if (snapshot) {
   await $`git push origin v${version} --no-verify`
   await $`git checkout dev`
   await $`git branch -D snapshot-${version}`
-  for await (const file of new Bun.Glob("**/package.json").scan({
-    absolute: true,
-  })) {
+  for (const file of pkgjsons) {
     $`await git checkout ${tree} ${file}`
   }
 }
