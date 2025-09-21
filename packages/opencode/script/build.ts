65a18700fa56f098ad30da01f0063c97ae78d2bf
Dax Raad <d@ironbay.co>
2025-09-21 02:36:50 -0400

    sync

--- packages/opencode/script/build.ts
+++ packages/opencode/script/build.ts
@@ -1,7 +1,11 @@
 #!/usr/bin/env bun
+
+import solidPlugin from "../../../node_modules/@opentui/solid/scripts/solid-plugin"
+
 const dir = new URL("..", import.meta.url).pathname
 process.chdir(dir)
 import { $ } from "bun"
+import path from "path"
 
 import pkg from "../package.json"
 
@@ -32,7 +36,14 @@ for (const [os, arch] of targets) {
   await $`CGO_ENABLED=0 GOOS=${os} GOARCH=${GOARCH[arch]} go build -ldflags="-s -w -X main.Version=${version}" -o ../opencode/dist/${name}/bin/tui ../tui/cmd/opencode/main.go`.cwd(
     "../tui",
   )
+  const opentui = `@opentui/core-${os === "windows" ? "win32" : os}-${arch.replace("-baseline", "")}`
+  await $`mkdir -p ../../node_modules/${opentui}`
+  await $`npm pack npm pack ${opentui}`.cwd(path.join(dir, "../../node_modules"))
+  await $`tar -xf ../../node_modules/${opentui.replace("@opentui/", "opentui-")}-*.tgz -C ../../node_modules/${opentui} --strip-components=1`
   await Bun.build({
+    conditions: ["browser"],
+    tsconfig: "./tsconfig.json",
+    plugins: [solidPlugin],
     compile: {
       target: `bun-${os}-${arch}` as any,
       outfile: `dist/${name}/bin/opencode`,
